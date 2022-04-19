a49fc5cde011001d73d79168c7b5f5862720dfb3
somebody-is <1697712297@qq.com>
2022-04-19 18:25:37 +0800

    项目初始化

---
+++ src/test/java/com/lzf/shirojwt/ShiroJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzf.shirojwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiroJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
